6b9751c020ed092f9cd7be6c652851178f0bd502
abma <spam@abma.de>
2010-11-16 01:05:47 +0100

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,8 @@
+#Test-mod of the Spring RTS engine
+
+## Readme
+
+This is the Testmod for the Spring RTS engine, to allow easy testing of recent changes in the engine.
+
+
+
---
+++ modinfo.lua
@@ -0,0 +1,14 @@
+return {
+	name='Balanced Annihilation',
+	description='Moooooo!',
+	shortname='BA',
+	version='modtest-V7.19',
+	mutator='Official',
+	game='Total Annihilation',
+	shortGame='TA',
+	modtype=1,
+	depend= {
+		'Balanced Annihilation V7.19'
+	},
+}
+
