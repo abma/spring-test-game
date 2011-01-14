41b50ce01c711868d056d5654d18b64ed8b9d98c
abma <spam@abma.de>
2011-01-14 08:40:00 +0100

    update to recent version of mod

--- modinfo.lua
+++ modinfo.lua
@@ -2,13 +2,13 @@ return {
 	name='Balanced Annihilation',
 	description='Moooooo!',
 	shortname='BA',
-	version='modtest-V7.19',
+	version='modtest-V7.20',
 	mutator='Official',
 	game='Total Annihilation',
 	shortGame='TA',
 	modtype=1,
 	depend= {
-		'Balanced Annihilation V7.19'
+		'Balanced Annihilation V7.20'
 	},
 }
 
